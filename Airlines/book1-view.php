a71b950337dc0ca535823af9d1190febb9a8fa19
UJFord <ujford@gmail.com>
2022-12-16 03:39:59 +0800

    book-view1 css style

--- Airlines/book1-view.php
+++ Airlines/book1-view.php
@@ -12,7 +12,9 @@ require 'admin/message.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> -->
+
+    <link rel="stylesheet" href="css/book1-view.css">
 
     <title>Passenger Data</title>
 
@@ -29,8 +31,8 @@ require 'admin/message.php';
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Passenger Details
-                        </h4>
+                        <!-- PROMPT double check entry -->
+                        <h4 id="prompt-head">Double Check Your Entries </h4>
                     </div>
                     <div class="card-body">
 
@@ -43,7 +45,7 @@ require 'admin/message.php';
 
                                 // echo
                         ?>
-                                <tr>
+                                <!-- <tr>
                                     <input type="hidden" name="passenger_id" value="<?= $passenger['passenger_id']; ?>">
                                     <td><?= $passenger['first_name']; ?></td>
                                     <td><?= $passenger['last_name']; ?></td>
@@ -55,10 +57,101 @@ require 'admin/message.php';
                                     <td>
                                         <a href="book2.php?passenger_id=<?= $passenger['passenger_id']; ?>" class="btn">Confirm</a>
                                         <form action="delete.php" method='POST' class="d-inline">
-                                            <button type="submit" name="delete_passenger" value="<?= $passenger['passenger_id']; ?>" class="btn">Cancel</a>
+                                            <button type="submit" name="delete_passenger" value="<?= $passenger['passenger_id']; ?>" class="btn">Cancel</button>
                                         </form>
                                     </td>
-                                </tr>
+                                </tr> -->
+
+                                <!-- main card -->
+                                <div id="main-card">
+
+                                    <!-- entry container -->
+                                    <div id="entry-container">
+
+                                        <!-- indv entries -->
+
+                                            <!-- fname -->
+                                            <div class="indv-container">
+
+                                                <!-- label -->
+                                                <h2 class="label">First Name:</h2>
+
+                                                <!-- entry -->
+                                                <h4 class="entry"><?= $passenger['first_name']; ?></h4>
+
+                                            </div>
+
+                                            <!-- lname -->
+                                            <div class="indv-container">
+
+                                                <!-- label -->
+                                                <h2 class="label">Last Name:</h2>
+
+                                                <!-- entry -->
+                                                <h4 class="entry"><?= $passenger['last_name']; ?></h4>
+
+                                            </div>
+                                            
+                                            <!-- date of birth -->
+                                            <div class="indv-container">
+
+                                                <!-- label -->
+                                                <h2 class="label">Date of Birth (yyyy-mm-dd):</h2>
+
+                                                <!-- entry -->
+                                                <h4 class="entry"><?= $passenger['date_of_birth']; ?></h4>
+
+                                            </div>
+                                            
+                                            <!-- citizenship -->
+                                            <div class="indv-container">
+
+                                                <!-- label -->
+                                                <h2 class="label">Citizenship:</h2>
+
+                                                <!-- entry -->
+                                                <h4 class="entry"><?= $passenger['citizenship']; ?></h4>
+
+                                            </div>
+                                            
+                                            <!-- phone -->
+                                            <div class="indv-container">
+
+                                                <!-- label -->
+                                                <h2 class="label">Phone Number:</h2>
+
+                                                <!-- entry -->
+                                                <h4 class="entry"><?= $passenger['p_number']; ?></h4>
+
+                                            </div>
+                                            
+                                            <!-- email -->
+                                            <div class="indv-container">
+
+                                                <!-- label -->
+                                                <h2 class="label">Email:</h2>
+
+                                                <!-- entry -->
+                                                <h4 class="entry"><?= $passenger['email']; ?></h4>
+
+                                            </div>
+
+                                    </div>
+
+                                    <!-- actions form -->
+                                    <form id="act-container"  action="delete.php" method='POST' class="d-inline">
+
+                                        <!-- confirm -->
+                                        <a id="confirm-btn" href="book2.php?passenger_id=<?= $passenger['passenger_id']; ?>">
+                                                Confirm
+                                        </a>
+
+                                        <!-- Cancel -->
+                                        <button id="cancel-btn" type="submit" name="delete_passenger" value="<?= $passenger['passenger_id']; ?>" class="btn" onclick="history.back()">Cancel</button>
+
+                                    </form>
+
+                                </div>
                         <?php
                             }
                         } else {
